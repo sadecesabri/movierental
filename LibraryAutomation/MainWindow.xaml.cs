5df7dad3ba3ff45883f83228a4fb78f83949e05a
Yağız Akyüz <yagizakyuz@gmail.com>
2018-03-04 18:35:40 +0300

    implemented security class for password hashing

--- LibraryAutomation/MainWindow.xaml.cs
+++ LibraryAutomation/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LibraryAutomation
 {
@@ -28,6 +17,12 @@ namespace LibraryAutomation
         private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
             Keyboard.Focus(textBoxID);
+        }
+
+        private void buttonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            String value = Security.Hash(textBoxPassword.Password);
+            Security.ValidatePassword(textBoxPassword.Password, value);
         }
     }
 }
---
+++ LibraryAutomation/Security.cs
@@ -0,0 +1,21 @@
+﻿using BCrypt;
+namespace LibraryAutomation
+{
+    static class Security
+    {
+        private static string GetRandomSalt()
+        {
+            return BCrypt.Net.BCrypt.GenerateSalt(12);
+        }
+
+        public static string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, GetRandomSalt());
+        }
+
+        public static bool ValidatePassword(string password, string correctHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, correctHash);
+        }
+    }
+}
